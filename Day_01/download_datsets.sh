7edb266182bfecbc6e03467ad7fbccd53054bffb
Jaemin Cho <heythisischo@gmail.com>
2017-07-25 03:31:24 +0900

    Add scripts

---
+++ Day_01/download_datsets.sh
@@ -0,0 +1,8 @@
+
+# naver sentiment corpus
+wget -O ./datasets/naver_train.txt https://raw.githubusercontent.com/e9t/nsmc/master/ratings_train.txt
+wget -O ./datasets/naver_test.txt https://raw.githubusercontent.com/e9t/nsmc/master/ratings_text.txt
+
+# namuwiki
+wget -O ./datasets/namuwiki170327.7z https://dataserver.xyz/files/namuwiki170327.7z
+7z x namuwiki170327.7z
