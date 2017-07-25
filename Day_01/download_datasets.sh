4ad26b48c9f2af700b8eed4289a02c10d4447cb7
Jaemin Cho <heythisischo@gmail.com>
2017-07-25 15:42:23 +0900

    Fix typo: text -> test

--- Day_01/download_datasets.sh
+++ Day_01/download_datasets.sh
@@ -1,7 +1,7 @@
 
 # naver sentiment corpus
 wget -O ./datasets/naver_train.txt https://raw.githubusercontent.com/e9t/nsmc/master/ratings_train.txt
-wget -O ./datasets/naver_test.txt https://raw.githubusercontent.com/e9t/nsmc/master/ratings_text.txt
+wget -O ./datasets/naver_test.txt https://raw.githubusercontent.com/e9t/nsmc/master/ratings_test.txt
 
 # namuwiki
 wget -O ./datasets/namuwiki170327.7z https://dataserver.xyz/files/namuwiki170327.7z
