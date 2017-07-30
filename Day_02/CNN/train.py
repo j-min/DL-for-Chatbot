f9166ff9573e60ee2899e6ac6562c41e6cd5332a
Jaemin Cho <heythisischo@gmail.com>
2017-07-30 21:10:23 +0900

    print configuration details

--- Day_02/CNN/train.py
+++ Day_02/CNN/train.py
@@ -1,9 +1,11 @@
 from solver import Solver
 from data_loader import get_loader
 from configs import get_config
+from pprint import pprint
 
 if __name__ == '__main__':
     config = get_config()
+    pprint(vars(config))
 
     data_loader = get_loader(
         batch_size=config.batch_size,
