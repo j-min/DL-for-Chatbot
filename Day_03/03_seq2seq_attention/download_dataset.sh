d21f4afc5d7962b47168fabb9bab238b68d904b6
Jaemin Cho <heythisischo@gmail.com>
2017-08-01 18:48:54 +0900

    Add seq2seq attention

---
+++ Day_03/03_seq2seq_attention/download_dataset.sh
@@ -0,0 +1,4 @@
+mkdir data
+wget http://www.manythings.org/anki/fra-eng.zip
+unzip fra-eng.zip
+mv fra.txt ./data/eng-fra.txt
---
+++ Day_03/03_seq2seq_attention/download_eng_kor.sh
@@ -0,0 +1,4 @@
+mkdir data
+wget http://www.manythings.org/anki/kor-eng.zip
+unzip kor-eng.zip
+mv kor.txt ./data/eng-kor.txt
